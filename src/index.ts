6ab9f5ab4e1e34a4950e9fc48d72b06f16f64d49
yasushi <yasushi@yasushiya.com>
2026-02-09 09:12:02 +0900

    chore: add CLI parse library ( cac )

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
-console.log("it works!");
+import { cac } from "cac";
+
+import pkg from "../package.json";
+
+const cli = cac("vegas");
+cli.version(pkg.version);
+
+cli.command("").action(() => console.log("it works!"));
+
+cli.help();
+cli.parse();
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
+  inlineOnly: ["cac"],
   outputOptions: {
     entryFileNames: "vegas.js",
   },
