c127b176b541e783bf531c4275c1044283537444
yasushi <yasushi@yasushiya.com>
2026-02-09 11:06:47 +0900

    chore: prepare cli subcommands

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,25 @@ import pkg from "../package.json";
 const cli = cac("vegas");
 cli.version(pkg.version);
 
-cli.command("").action(() => console.log("it works!"));
+// serve
+cli
+  .command("[root]")
+  .alias("serve")
+  .alias("dev")
+  .action((root?: string) => {
+    console.log("it works!");
+    console.log(`root is ${root}`);
+  });
+
+// build
+cli.command("build [root]").action((_root?: string) => {
+  console.log("This feature is under development!");
+});
+
+// preview
+cli.command("preview [root]").action((_root?: string) => {
+  console.log("This feature is under development!");
+});
 
 cli.help();
 cli.parse();
