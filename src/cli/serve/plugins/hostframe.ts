1f265a8eb9f8ffb1afa22269e62783478e18dc84
yasushi <yasushi@yasushiya.com>
2026-02-22 23:29:28 +0900

    chore: impl parts of GAS API

--- src/cli/serve/plugins/hostframe.ts
+++ src/cli/serve/plugins/hostframe.ts
@@ -10,8 +10,118 @@ import { exportBridge } from "../../build/plugins/exportbridge";
 import { virtualHTML } from "../../build/plugins/virtualhtml";
 import { ResolvedUserConfig } from "../../config";
 
+// https://developers.google.com/apps-script/reference/html/html-output-meta-tag
+class GASHtmlOutputMetaTag implements GoogleAppsScript.HTML.HtmlOutputMetaTag {
+  readonly #name: string;
+  readonly #content: string;
+
+  constructor(name: string, content: string) {
+    this.#name = name;
+    this.#content = content;
+  }
+
+  getContent(): string {
+    return this.#content;
+  }
+  getName(): string {
+    return this.#name;
+  }
+}
+
+// https://developers.google.com/apps-script/reference/html/html-output
+class GASHtmlOutput implements GoogleAppsScript.HTML.HtmlOutput {
+  #title: string = "";
+  #faviconUrl: string = "";
+  #content: string = "";
+  readonly #metaTags: GASHtmlOutputMetaTag[] = [];
+  readonly #allowedMetaTags: readonly string[] = [
+    "apple-mobile-web-app-capable",
+    "google-site-verification",
+    "mobile-web-app-capable",
+    "viewport",
+  ];
+
+  addMetaTag(name: string, content: string): GoogleAppsScript.HTML.HtmlOutput {
+    if (this.#allowedMetaTags.includes(name)) {
+      this.#metaTags.push(new GASHtmlOutputMetaTag(name, content));
+    }
+    return this;
+  }
+  append(addedContent: string): GoogleAppsScript.HTML.HtmlOutput {
+    this.#content += addedContent;
+    return this;
+  }
+  appendUntrusted(addedContent: string): GoogleAppsScript.HTML.HtmlOutput {
+    // TODO: Need to brush up on logic
+    return this.append(addedContent.replaceAll("<", "&lt;").replaceAll(">", "&gt;"));
+  }
+  asTemplate(): GoogleAppsScript.HTML.HtmlTemplate {
+    throw new Error("Method not implemented.");
+  }
+  clear(): GoogleAppsScript.HTML.HtmlOutput {
+    this.#content = "";
+    return this;
+  }
+  getAs(_contentType: string): GoogleAppsScript.Base.Blob {
+    throw new Error("Method not implemented.");
+  }
+  getBlob(): GoogleAppsScript.Base.Blob {
+    throw new Error("Method not implemented.");
+  }
+  getContent(): string {
+    return this.#content;
+  }
+  getFaviconUrl(): string {
+    return this.#faviconUrl;
+  }
+  getHeight(): GoogleAppsScript.Integer {
+    // If published in a web app, it always returns null.
+    return null as unknown as GoogleAppsScript.Integer;
+  }
+  getMetaTags(): GoogleAppsScript.HTML.HtmlOutputMetaTag[] {
+    return this.#metaTags;
+  }
+  getTitle(): string {
+    return this.#title;
+  }
+  getWidth(): GoogleAppsScript.Integer {
+    // If published in a web app, it always returns null.
+    return null as unknown as GoogleAppsScript.Integer;
+  }
+  setContent(content: string): GoogleAppsScript.HTML.HtmlOutput {
+    this.#content = content;
+    return this;
+  }
+  setFaviconUrl(iconUrl: string): GoogleAppsScript.HTML.HtmlOutput {
+    this.#faviconUrl = iconUrl;
+    return this;
+  }
+  setHeight(_height: GoogleAppsScript.Integer): GoogleAppsScript.HTML.HtmlOutput {
+    // Calling this method has no effect when published in a web app.
+    return this;
+  }
+  setSandboxMode(_mode: GoogleAppsScript.HTML.SandboxMode): GoogleAppsScript.HTML.HtmlOutput {
+    // Only IFRAME mode is now supported.
+    return this;
+  }
+  setTitle(title: string): GoogleAppsScript.HTML.HtmlOutput {
+    this.#title = title;
+    return this;
+  }
+  setWidth(_width: GoogleAppsScript.Integer): GoogleAppsScript.HTML.HtmlOutput {
+    // Calling this method has no effect when published in a web app.
+    return this;
+  }
+  setXFrameOptionsMode(
+    _mode: GoogleAppsScript.HTML.XFrameOptionsMode,
+  ): GoogleAppsScript.HTML.HtmlOutput {
+    throw new Error("Method not implemented.");
+  }
+}
+
+// https://developers.google.com/apps-script/reference/html/html-service
 class GASHtmlService implements GoogleAppsScript.HTML.HtmlService {
-  #webCodeMap: Map<string, string>;
+  readonly #webCodeMap: Map<string, string>;
 
   constructor(webCodeMap: Map<string, string>) {
     this.#webCodeMap = webCodeMap;
@@ -30,78 +140,6 @@ class GASHtmlService implements GoogleAppsScript.HTML.HtmlService {
   createHtmlOutput(html?: unknown): GoogleAppsScript.HTML.HtmlOutput {
     if (typeof html !== "string") {
       throw new Error("Method not implemented.");
-    }
-
-    class GASHtmlOutput implements GoogleAppsScript.HTML.HtmlOutput {
-      #title: string = "";
-      #faviconUrl: string = "";
-      #content: string = "";
-
-      addMetaTag(_name: string, _content: string): GoogleAppsScript.HTML.HtmlOutput {
-        throw new Error("Method not implemented.");
-      }
-      append(_addedContent: string): GoogleAppsScript.HTML.HtmlOutput {
-        throw new Error("Method not implemented.");
-      }
-      appendUntrusted(_addedContent: string): GoogleAppsScript.HTML.HtmlOutput {
-        throw new Error("Method not implemented.");
-      }
-      asTemplate(): GoogleAppsScript.HTML.HtmlTemplate {
-        throw new Error("Method not implemented.");
-      }
-      clear(): GoogleAppsScript.HTML.HtmlOutput {
-        throw new Error("Method not implemented.");
-      }
-      getAs(_contentType: string): GoogleAppsScript.Base.Blob {
-        throw new Error("Method not implemented.");
-      }
-      getBlob(): GoogleAppsScript.Base.Blob {
-        throw new Error("Method not implemented.");
-      }
-      getContent(): string {
-        return this.#content;
-      }
-      getFaviconUrl(): string {
-        return this.#faviconUrl;
-      }
-      getHeight(): GoogleAppsScript.Integer {
-        throw new Error("Method not implemented.");
-      }
-      getMetaTags(): GoogleAppsScript.HTML.HtmlOutputMetaTag[] {
-        throw new Error("Method not implemented.");
-      }
-      getTitle(): string {
-        return this.#title;
-      }
-      getWidth(): GoogleAppsScript.Integer {
-        throw new Error("Method not implemented.");
-      }
-      setContent(content: string): GoogleAppsScript.HTML.HtmlOutput {
-        this.#content = content;
-        return this;
-      }
-      setFaviconUrl(iconUrl: string): GoogleAppsScript.HTML.HtmlOutput {
-        this.#faviconUrl = iconUrl;
-        return this;
-      }
-      setHeight(_height: GoogleAppsScript.Integer): GoogleAppsScript.HTML.HtmlOutput {
-        throw new Error("Method not implemented.");
-      }
-      setSandboxMode(_mode: GoogleAppsScript.HTML.SandboxMode): GoogleAppsScript.HTML.HtmlOutput {
-        throw new Error("Method not implemented.");
-      }
-      setTitle(title: string): GoogleAppsScript.HTML.HtmlOutput {
-        this.#title = title;
-        return this;
-      }
-      setWidth(_width: GoogleAppsScript.Integer): GoogleAppsScript.HTML.HtmlOutput {
-        throw new Error("Method not implemented.");
-      }
-      setXFrameOptionsMode(
-        _mode: GoogleAppsScript.HTML.XFrameOptionsMode,
-      ): GoogleAppsScript.HTML.HtmlOutput {
-        throw new Error("Method not implemented.");
-      }
     }
     const output = new GASHtmlOutput();
     return output.setContent(html);
@@ -276,6 +314,17 @@ export function hostFrame(config: ResolvedUserConfig, projectEntry: ProjectEntry
             defaultTreeAdapter.setDocumentType(document, "html", "", "");
             const htmlTag = defaultTreeAdapter.createElement("html", html.NS.HTML, []);
             const headTag = defaultTreeAdapter.createElement("head", html.NS.HTML, []);
+
+            const htmlOutputMetaTags = htmlOutput.getMetaTags();
+            if (htmlOutputMetaTags.length > 0) {
+              htmlOutputMetaTags.forEach((metaTag) => {
+                const meta = defaultTreeAdapter.createElement("meta", html.NS.HTML, [
+                  { name: "name", value: metaTag.getName() },
+                  { name: "content", value: metaTag.getContent() },
+                ]);
+                defaultTreeAdapter.appendChild(headTag, meta);
+              });
+            }
 
             const htmlOutputTitle = htmlOutput.getTitle();
             if (htmlOutputTitle) {
