a63eca3fb057cddd88e81e95dd4b21e0494b7ac2
yasushi <yasushi@yasushiya.com>
2026-02-27 12:11:06 +0900

    chore: path resolve logic

--- src/core/path.ts
+++ src/core/path.ts
@@ -1,11 +1,12 @@
 import fs from "node:fs";
 import path from "node:path";
 
-export function resolvePath(rawPath?: string) {
-  const absolutePath = path.resolve(rawPath ?? process.cwd());
-  if (!fs.existsSync(absolutePath)) {
+export function resolvePath(rawPath: string = "."): string {
+  const absolutePath = path.isAbsolute(rawPath) ? rawPath : path.join(process.cwd(), rawPath);
+  const resolvedPath = path.resolve(absolutePath);
+  if (!fs.existsSync(resolvedPath)) {
     throw new Error("The directory or file does not exist.");
   }
 
-  return absolutePath;
+  return resolvedPath;
 }
