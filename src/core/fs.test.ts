7bb4d1e39a4b90d244b5bf5d21b2a7b108a490a5
yasushi <yasushi@yasushiya.com>
2026-03-02 11:05:45 +0900

    chore: use memfs when test

--- src/core/fs.test.ts
+++ src/core/fs.test.ts
@@ -1,15 +1,39 @@
-import fs from "node:fs";
+import os from "node:os";
 import path from "node:path";
 
-import { describe, expect, test } from "vitest";
+import { fs, vol } from "memfs";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 
 import { DisposableTempDir } from "./fs";
+
+vi.mock("node:fs", () => {
+  return {
+    default: fs,
+  };
+});
+
+beforeEach(() => {
+  vol.reset();
+  vol.mkdirSync(os.tmpdir(), { recursive: true });
+  vol.mkdirSync(path.join(process.cwd(), "node_modules"), { recursive: true });
+});
 
 test("the temp directory path is generated.", () => {
   const name = "test";
   using tempDir = new DisposableTempDir(name);
 
   const expectTempPath = path.join(process.cwd(), "node_modules", "test-");
+
+  expect(tempDir.getPath().startsWith(expectTempPath)).toBe(true);
+});
+
+test("the temp directory path is generated if `node_modules` is not exists.", () => {
+  fs.rmSync("node_modules", { recursive: true, force: true });
+
+  const name = "test";
+  using tempDir = new DisposableTempDir(name);
+
+  const expectTempPath = path.join(os.tmpdir(), "test-");
 
   expect(tempDir.getPath().startsWith(expectTempPath)).toBe(true);
 });
