b9ee20386ad5d4b5ce8d3ea8474c783488b66e3e
yasushi <yasushi@yasushiya.com>
2026-03-02 22:58:34 +0900

    chore: shape up settings

--- packages/create-vegas/tsdown.config.ts
+++ packages/create-vegas/tsdown.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig([
-  {
-    entry: "./src",
-    inlineOnly: ["cac", "@clack/core", "@clack/prompts", "picocolors", "sisteransi"],
-    outputOptions: {
-      entryFileNames: "create-vegas.js",
-    },
-    dts: false,
+export default defineConfig({
+  entry: { "create-vegas": "./src" },
+  inlineOnly: ["cac", "@clack/core", "@clack/prompts", "picocolors", "sisteransi"],
+  outputOptions: {
+    entryFileNames: "[name].js",
   },
-]);
+  dts: false,
+});
