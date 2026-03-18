3c6dc0c9d842a728e10f704d9a1a35e4a6a35834
yasushi <yasushi@yasushiya.com>
2026-03-18 11:53:59 +0900

    chore: reaction template view

---
+++ packages/create-vegas/template-vue/src/web/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <section>
+    <div>Get started</div>
+    <p>Edit <code>src/App.vue</code> and save to test the blazing fast <code>FBR</code></p>
+    <button @click="count++">Count is {{ count }}</button>
+  </section>
+</template>
