227c87541a82fe0870cad53b9daa543b0301315e
yasushi <yasushi@yasushiya.com>
2026-04-03 17:43:41 +0900

    chore: strict type separation between client and server

--- packages/vegas/client.d.ts
+++ packages/vegas/client.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="vite/client" />
-/// <reference types="google-apps-script" />
 /// <reference types="./types/importMeta" />
 export * from "./dist/lib";
---
+++ packages/vegas/server.d.ts
@@ -0,0 +1,1 @@
+/// <reference types="google-apps-script" />
