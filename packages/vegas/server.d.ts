8e6f41b026c59e188bab36f37db005bcac34522f
yasushi <yasushi@yasushiya.com>
2026-04-05 16:29:52 +0900

    chore: improve console arg types

--- packages/vegas/server.d.ts
+++ packages/vegas/server.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="google-apps-script" />
+interface ImportMetaEnv {
+  DEV: boolean;
+  PROD: boolean;
+  MODE: "development" | "production";
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+interface Console {
+  log(formatOrObject: unknown, ...values: unknown[]): void;
+  info(formatOrObject: unknown, ...values: unknown[]): void;
+  warn(formatOrObject: unknown, ...values: unknown[]): void;
+  error(formatOrObject: unknown, ...values: unknown[]): void;
+}
