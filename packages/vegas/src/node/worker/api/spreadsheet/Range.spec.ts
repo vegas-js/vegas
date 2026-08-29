ec72450a81f4b5c32a92ac021326d1a3cd84d0ac
yasushi <yasushi@yasushiya.com>
2026-08-29 17:42:33 +0900

    chore: improve boundary test

--- packages/vegas/src/node/worker/api/spreadsheet/Range.spec.ts
+++ packages/vegas/src/node/worker/api/spreadsheet/Range.spec.ts
@@ -3,13 +3,17 @@ import { describe, expect, test } from "vitest";
 import { Range } from "./Range";
 
 describe("getCell", () => {
-  test("check args bundary", () => {
-    const range = new Range("", 0, 100, 100, 100, 100, () => {});
+  test("check argument boundaries", () => {
+    const range = new Range("", 0, 1, 1, 100, 100, () => {});
 
-    expect(range.getCell(1, 1));
+    // minimum boundaries
+    expect(() => range.getCell(1, 1)).not.toThrow();
     expect(() => range.getCell(0, 1)).toThrow("out of range.");
     expect(() => range.getCell(1, 0)).toThrow("out of range.");
-    expect(() => range.getCell(999, 1)).toThrow("out of range.");
-    expect(() => range.getCell(1, 999)).toThrow("out of range.");
+
+    // maximum boundaries
+    expect(() => range.getCell(100, 100)).not.toThrow();
+    expect(() => range.getCell(101, 100)).toThrow("out of range.");
+    expect(() => range.getCell(100, 101)).toThrow("out of range.");
   });
 });
