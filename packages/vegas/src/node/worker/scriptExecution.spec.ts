38617a168811d6329c08b2354a2d2c51231aeb98
yasushi <yasushi@yasushiya.com>
2026-08-31 09:10:28 +0900

    add: scriptExecution test

---
+++ packages/vegas/src/node/worker/scriptExecution.spec.ts
@@ -0,0 +1,105 @@
+import { expect, test } from "vitest";
+
+import { HtmlOutput } from "./api/html/HtmlOutput";
+import { invokeScriptFunction } from "./invocation";
+import type { ScriptContextDependencies } from "./scriptContext";
+import { createScriptContext } from "./scriptContext";
+import { evaluateScript } from "./scriptRuntime";
+
+const unexpected = () => {
+  throw new Error("Unexpected dependency call");
+};
+
+function createContext(overrides: Partial<ScriptContextDependencies> = {}) {
+  return createScriptContext({
+    requestLegacySync: unexpected,
+
+    createFile: unexpected,
+    createFolder: unexpected,
+    createHtmlOutput: unexpected,
+    createHtmlTemplate: unexpected,
+    createSpreadsheet: unexpected,
+
+    sessionService: {
+      getActiveUser: unexpected,
+      getEffectiveUser: unexpected,
+      getActiveUserLocale: unexpected,
+      getScriptTimeZone: unexpected,
+      getTemporaryActiveUserKey: unexpected,
+    },
+
+    cacheService: {
+      get: unexpected,
+      getAll: unexpected,
+      put: unexpected,
+      putAll: unexpected,
+      remove: unexpected,
+      removeAll: unexpected,
+    },
+
+    propertiesService: {
+      deleteAllProperties: unexpected,
+      deleteProperty: unexpected,
+      getKeys: unexpected,
+      getProperties: unexpected,
+      getProperty: unexpected,
+      setProperties: unexpected,
+      setProperty: unexpected,
+    },
+    ...overrides,
+  });
+}
+
+test("user code accessible to typed service", async () => {
+  const context = createContext({
+    sessionService: {
+      getActiveUser: () => "active@example.com",
+      getEffectiveUser: () => "effective@example.com",
+      getActiveUserLocale: () => "ja",
+      getScriptTimeZone: () => "Asia/Tokyo",
+      getTemporaryActiveUserKey: () => "-- Active user key --",
+    },
+  });
+  evaluateScript(
+    `
+    function execute(name) {
+      return {
+        name,
+        email: Session.getActiveUser().getEmail(),
+        locale: Session.getActiveUserLocale(),
+        timeZone: Session.getScriptTimeZone(),
+      };
+    }`,
+    context,
+  );
+  const result = await invokeScriptFunction(context, "execute", ["Vegas"]);
+
+  expect(result).toEqual({
+    name: "Vegas",
+    email: "active@example.com",
+    locale: "ja",
+    timeZone: "Asia/Tokyo",
+  });
+});
+
+test("return a value from doGet to the actual GAS global", async () => {
+  const context = createContext({
+    createHtmlOutput: (content: string, mode: GoogleAppsScript.HTML.XFrameOptionsMode) =>
+      new HtmlOutput(content, mode),
+  });
+  evaluateScript(
+    `
+    function doGet() {
+      return HtmlService
+        .createHtmlOutput("<h1>Hello</h1>")
+        .setTitle("Hello");
+    }`,
+    context,
+  );
+  const result = await invokeScriptFunction(context, "doGet", []);
+
+  expect(result).toMatchObject({
+    title: "Hello",
+    content: "<h1>Hello</h1>",
+  });
+});
