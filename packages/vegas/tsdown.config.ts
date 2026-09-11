0ec04f53bd2ff64890463ae4fd6adcc4991311d2
yasushi <yasushi@yasushiya.com>
2026-09-11 17:11:36 +0900

    chore: adjust validate files

--- packages/vegas/tsdown.config.ts
+++ packages/vegas/tsdown.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "tsdown";
 export default defineConfig([
   {
     entry: "src/index.ts",
+    attw: {
+      profile: "esm-only",
+    },
     dts: {
       compilerOptions: {
         isolatedDeclarations: true,
@@ -13,5 +16,6 @@ export default defineConfig([
   {
     entry: "src/cli.ts",
     fixedExtension: false,
+    dts: false,
   },
 ]);
