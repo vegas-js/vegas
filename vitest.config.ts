117eb2d7d9d23ac04606b98bc984cb62e84ccd76
yasushi <yasushi@yasushiya.com>
2026-09-14 09:42:59 +0900

    chore: set true fsModuleCache

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    fsModuleCache: true,
     coverage: {
       include: ["packages/*/src/**/*.ts"],
       reporter: "text",
