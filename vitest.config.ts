eaf8e4ad65bc1cb1d8f14d88fa4fb324fe677252
yasushi <yasushi@yasushiya.com>
2026-09-16 08:23:12 +0900

    chore: adjust test peformance

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     fsModuleCache: true,
+    isolate: false,
     coverage: {
       include: ["packages/*/src/**/*.ts"],
       reporter: "text",
