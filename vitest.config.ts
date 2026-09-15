4bf914242ed98208cd8d9b80f3e4540c22f73fcd
yasushi <yasushi@yasushiya.com>
2026-09-16 08:25:25 +0900

    Revert "chore: adjust test peformance"
    This reverts commit eaf8e4ad65bc1cb1d8f14d88fa4fb324fe677252.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     fsModuleCache: true,
-    isolate: false,
     coverage: {
       include: ["packages/*/src/**/*.ts"],
       reporter: "text",
