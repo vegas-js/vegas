99f64003220c7da89508c9d86fb0350f73563722
yasushi <yasushi@yasushiya.com>
2026-09-18 01:03:27 +0900

    refactor: remove legacy session runtime

--- packages/vegas/src/node/cli/core/handlers/index.ts
+++ packages/vegas/src/node/cli/core/handlers/index.ts
@@ -1,6 +1,5 @@
 export { HtmlServiceHandler } from "./htmlService";
 export { RangeHandler } from "./range";
-export { SessionHandler } from "./session";
 export { SheetHandler } from "./sheet";
 export { SpreadsheetAppHandler } from "./spreadsheetApp";
 export { UrlFetchAppHandler } from "./urlFetchApp";
