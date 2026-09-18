220f3e569c8ebc2fbec3f2e111a72785953858bd
yasushi <yasushi@yasushiya.com>
2026-09-18 20:53:28 +0900

    refactor: remove legacy url fetch runtime

--- packages/vegas/src/node/cli/core/handlers/index.ts
+++ packages/vegas/src/node/cli/core/handlers/index.ts
@@ -1,4 +1,3 @@
 export { RangeHandler } from "./range";
 export { SheetHandler } from "./sheet";
 export { SpreadsheetAppHandler } from "./spreadsheetApp";
-export { UrlFetchAppHandler } from "./urlFetchApp";
