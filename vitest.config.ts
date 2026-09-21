3f2694f05661506e8407a4e9dc434621998daded
yasushi <yasushi@yasushiya.com>
2026-09-21 16:18:16 +0900

    chore: simplify local test output

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     fsModuleCache: true,
+    reporters: process.env.GITHUB_ACTIONS === "true" ? undefined : ["dot"],
     coverage: {
       include: ["packages/*/src/**/*.ts"],
       reporter: "text",
